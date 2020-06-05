9e46f8737f9fb75e3ccf3e713f480fe33a5e9c7c
patrick <patrick204nqh@gmail.com>
2020-06-05 23:37:47 +0700

    controllers

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pizzas', function () {
-    // get data from db
-
-    $pizzas = [
-        ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-        ['type' => 'volcano', 'base' => 'garlic crust'],
-        ['type' => 'veg supreme', 'base' => 'thin & crispy']
-    ];
-
-    return view('pizzas', [
-        'pizzas' => $pizzas,
-    ]);
-});
-
-Route::get('/pizzas/{id}', function ($id) {
-    // use the $id variable to query the db for a record
-    return view('details', ['id' => $id]);
-});
+Route::get('/pizzas', 'PizzaController@index');
+Route::get('/pizzas/{id}', 'PizzaController@show');
