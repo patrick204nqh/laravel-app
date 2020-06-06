443692bb574c44998a7ed088cca9ac0542e2ee1d
patrick <patrick204nqh@gmail.com>
2020-06-06 14:35:31 +0700

    authentication views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     You are logged in!
+                    <p><a href="/pizzas">View all pizza orders</a></p>
                 </div>
             </div>
         </div>
