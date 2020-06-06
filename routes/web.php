47d4f7b0175b0ce9611b2debce79427c18fc83d9
patrick <patrick204nqh@gmail.com>
2020-06-06 14:45:40 +0700

    disabling registration

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('/pizzas/create', 'PizzaController@create');
 Route::post('/pizzas', 'PizzaController@store');
 Route::get('/pizzas/{id}', 'PizzaController@show')->middleware('auth');
 Route::delete('/pizzas/{id}', 'PizzaController@destroy')->middleware('auth');
-Auth::routes();
+
+Auth::routes([
+    'register' => false
+]);
 
 Route::get('/home', 'HomeController@index')->name('home');
